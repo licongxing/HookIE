14344cdeb185fc17f06a0ef8ffe29c2624ca75b2
licongxing <congxingli@qq.com>
2019-12-16 18:48:38 +0800

    第一次添加 Signed-off-by: licongxing <congxingli@qq.com>

---
+++ HookIE/HookIE/InjectDll.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <list>
+
+class CInjectDll
+{
+public:
+	CInjectDll(void);
+	~CInjectDll(void);
+
+	// 获取进程句柄所在的路径
+	static CString GetModulePath(HMODULE hModule = NULL);
+
+	// 获取指定EXE路径的进程句柄
+	static void GetProcessHandle(CString strExePath,std::list<HANDLE> &handleList);
+	
+	// 指定DLL注入到指定EXE进程
+	static void InjectDllToExe(CString strDllPath,CString strExePath);
+
+	// 指定DLL注入到指定进程句柄
+	static bool InjectDllToProc(CString strDllPath, HANDLE targetProc);
+
+	// 指定DLL从指定EXE进程卸载
+	static void UninstallDllToExe(CString strDllPath,CString strExePath);
+
+	// 指定DLL从指定进程句柄卸载
+	static bool  UninstallDllToProc(CString strDllPath, HANDLE targetProc);
+
+};
+
